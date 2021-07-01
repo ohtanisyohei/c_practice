cf6b37bf22e6ca86398c16df6310d4bb9c1772da
ohtanisyohei <86642868+ohtanisyohei@users.noreply.github.com>
2021-07-01 15:30:16 +0900

    Add files via upload

---
+++ exercise01.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+printf("‚¨‚Í‚æ‚¤\n‚±‚ñ‚É‚¿‚Í\n‚±‚ñ‚Î‚ñ‚Í\n");
+
+return 0;
+}
+
---
+++ exercise02.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	char lang = 'C';
+	int year=2005;
+	double point=13.2;
+	printf("%d年以降で%c言語の人気が一番高かった時のポイントは%.1lf%",year,lang,point);
+
+return 0;
+}
+
